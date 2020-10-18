397d601909decae345c1b8646903287eec2c1361
CanKRVN <erdacan54@gmail.com>
2020-10-18 19:31:01 +0300

    last commit
    proje bu şekilde teslim edilmek üzere githuba yüklenmiştir.

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.PackageManager.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour
+{
+ 
+    public void Restart()
+    {
+        SceneManager.LoadScene("SampleScene");
+        
+    }
+
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit Game");
+
+    }
+  
+ 
+    public void StartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
